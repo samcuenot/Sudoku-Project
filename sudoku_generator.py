6e12772970a7bce2a5cc3cbb58d01415f7afaab7
haileymair <haileymair@ufl.edu>
2025-04-15 13:33:50 -0400

    started Board(not done)

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -231,3 +231,33 @@ class Cell:
 
     def draw(self):
         pass
+
+class Board:
+    def __init__(self,width,height,screen,difficulty):
+        self.width = width
+        self.height = height
+        self.screen = screen
+        self.difficulty = difficulty
+    def draw(self):
+        pass
+    def select(self,row,col):
+        self.row = row
+        self.col = col
+    def click(self, row, col):
+        pass
+    def clear(self):
+        pass
+    def sketch(self,value):
+        pass
+    def place_number(self,value):
+        pass
+    def reset_to_original(self):
+        pass
+    def is_full(self):
+        pass
+    def update_board(selfself):
+        pass
+    def find_empty(self):
+        pass
+    def check_board(self):
+        pass
