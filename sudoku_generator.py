fb11d63e338bd80054d5a3a9ae117ce27201e7ee
haileymair <haileymair@ufl.edu>
2025-04-15 13:08:00 -0400

    example

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -27,6 +27,7 @@ class SudokuGenerator:
         self.removed_cells = removed_cells
         self.board = [[0 for _ in range(row_length)] for _ in range(row_length)]
         self.box_length = int(math.sqrt(row_length))
+        self.example = 2
 
 
     '''
